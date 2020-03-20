54a18fe83416a5e2381d3e65926780d002234f20
Callan McGill <callan.mcgill@gmail.com>
2020-03-20 15:54:18 -0400

    Make more friendly for ffi usage by returning result and change length convention

--- simd.c
+++ simd.c
@@ -4,25 +4,25 @@
 #include <stdint.h>
 #include <inttypes.h>
 
-void mmfitch (int len, uint64_t* bv1, uint64_t* bv2, uint64_t* res);
-int f (int);
+uint64_t* mmfitch (int len, const uint64_t* bv1, const uint64_t* bv2);
 
 int main (){
-  uint64_t l[]   __attribute__((aligned (16))) = {3, 7, 3, 11, 3};
-  uint64_t r[]   __attribute__((aligned (16))) = {2, 5, 3, 15, 2};
-  uint64_t res[] __attribute__((aligned (16))) = {0, 0, 0, 0 , 0};
+  uint64_t l[]   __attribute__((aligned (16))) = {3, 7, 3, 11, 0, 3};
+  uint64_t r[]   __attribute__((aligned (16))) = {2, 5, 3, 15, 0, 4};
+  uint64_t* res  __attribute__((aligned (16)));
   //  res = &l[0];
-  int len = 3;
-  mmfitch(len, &l[0], &r[0], &res[0]);
-  for (int i = 0; i < f(len); i ++){
+  int len = 6;
+  res = mmfitch(len, &l[0], &r[0]);
+  for (int i = 0; i < len; i ++){
     printf("%" PRIu64 " ", res[i]);
     }
   printf("\n");
 }
 
-
-void mmfitch (int len, uint64_t* bv1, uint64_t* bv2, uint64_t* res){
-  for (int i = 0; i < len; i++){
+uint64_t* mmfitch (int len, const uint64_t* bv1, const uint64_t* bv2){
+  uint64_t* res  __attribute__((aligned (16))) = malloc(len);
+  uint64_t* start = res;
+  for (int i = 0; i < (len + 1) / 2; i++){
     __m128i lvals = _mm_loadu_si128((__m128i*) bv1);
     __m128i rvals = _mm_loadu_si128((__m128i*) bv2);
     __m128i __mres;
@@ -31,7 +31,7 @@ void mmfitch (int len, uint64_t* bv1, uint64_t* bv2, uint64_t* res){
       __mres = _mm_and_si128(lvals, rvals);
       }
     else {
-      // otherwise take the union,
+      // otherwise take the union.
       __mres = _mm_or_si128 (lvals, rvals);
     }
     _mm_store_si128((__m128i*) res, __mres);
@@ -39,16 +39,10 @@ void mmfitch (int len, uint64_t* bv1, uint64_t* bv2, uint64_t* res){
     bv2 +=2;
     res +=2;
   }
+  return start;
 }
 
-int f (int num) {
-  if (num%2==0){
-    return num*2;
-  }
-  else {
-    return num*2 - 1;
-  }
-}
+
     
   
   
